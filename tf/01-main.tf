ef10f6b032d0cff2b0359fb9c0ca9af0df47082c
mariohuq <15021607+mariohuq@users.noreply.github.com>
2025-03-18 12:48:49 +0300

    try 2

--- tf/01-main.tf
+++ tf/01-main.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 0.14.0"
   required_providers {
     openstack = {
-      source = "yandex-cloud/openstack"
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.39. 0"
     }
   }
 }
