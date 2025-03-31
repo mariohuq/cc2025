1cc4166e5f454387f4e42d69fe2d5d6349c38758
mariohuq <15021607+mariohuq@users.noreply.github.com>
2025-03-31 22:22:53 +0300

    try 9

--- tf-yandex/yc_vars.sh
+++ tf-yandex/yc_vars.sh
@@ -1,9 +1,9 @@
 #/usr/bin/env bash
 
-# ~/yandex-cloud/bin/
-YC_TOKEN=$(yc iam create-token)
-YC_CLOUD_ID=$(yc config get cloud-id)
-YC_FOLDER_ID=$(yc config get folder-id)
+# 
+YC_TOKEN=$($HOME/yandex-cloud/bin/yc iam create-token)
+YC_CLOUD_ID=$($HOME/yandex-cloud/bin/yc config get cloud-id)
+YC_FOLDER_ID=$($HOME/yandex-cloud/bin/yc config get folder-id)
 
 echo $(jq --null-input \
           --arg token "$YC_TOKEN" \
